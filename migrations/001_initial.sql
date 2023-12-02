478ab27f44a6af058c758eb963aa0f728c397772
the-kwisatz-haderach <gustaflundstrom90@gmail.com>
2023-12-02 10:07:28 +0100

    Add vs code sql formatter and apply

--- migrations/001_initial.sql
+++ migrations/001_initial.sql
@@ -1,20 +1,25 @@
 -- Write your migrate up statements here
-create table recipes (
-  id integer primary key generated always as identity,
-  recipe_name varchar(255)
-);
+create table
+  recipes (
+    id int generated always as identity,
+    recipe_name varchar(255),
+    primary key (id),
+    user_id int
+  );
 
-create table users (
-  id integer primary key generated always as identity,
-  username varchar(255),
-  password varchar(255) not null,
-  email varchar(255) not null
+create table
+  users (
+    id int primary key generated always as identity,
+    username varchar(255),
+    password varchar(255) not null,
+    email varchar(255) not null unique
+  );
 
-)
+alter table recipes add foreign key (user_id) references users (id);
 
 ---- create above / drop below ----
-
 drop table recipes;
+
 drop table users;
 
 -- Write your migrate down statements here. If this migration is irreversible
